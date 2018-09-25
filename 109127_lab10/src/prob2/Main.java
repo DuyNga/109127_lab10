4eb38984998c319376e363cdb86053089e4c64b5
duynga <loduynga@gmail.com>
2018-09-24 20:57:04 -0500

    commit last

--- 109127_lab10/src/prob2/Main.java
+++ 109127_lab10/src/prob2/Main.java
@@ -1,20 +1,29 @@
 package prob2;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Main {
 	public static <T> T secondSmallest(List<T> list) {
 		if (list.size() <= 1)
 			return null;
-		Stream<T> str = list.stream().distinct().sorted();
-		if (str.count() == 1)
+		Supplier<Stream<T>> sup = () -> list.stream().distinct().sorted();
+		if (sup.get().count() == 1)
 			return null;
-
-		return str.skip(1).findFirst().get();
+		T re = sup.get().skip(1).findFirst().get();
+		return re;
 	}
-	
+
 	public static void main(String[] args) {
-		
+		List<Integer> ints = Arrays.asList(1, 2, 2, 3, 3, 4);
+		System.out.println("List: " + ints);
+		Integer intSecondSmallest = secondSmallest(ints);
+		System.out.println("Second smallest in list: " + intSecondSmallest);
+		List<Double> doubles = Arrays.asList(2.0, 2.0, 3.0, 3.0, 4.0);
+		System.out.println("List: " + doubles);
+		double dbSecondSmallest = secondSmallest(doubles);
+		System.out.println("Second smallest in list: " + dbSecondSmallest);
 	}
 }
--- 109127_lab10/src/prob3/Main.java
+++ 109127_lab10/src/prob3/Main.java
@@ -1,19 +1,26 @@
 package prob3;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-public static <T extends Number> double  sum (List<T> list) {
-	double result =0.0;
-	for(T a:list) {
-		result += a.doubleValue();
-	}
-	return result;
+	public static <T extends Number> double sum(List<T> list) {
+		double result = 0.0;
+		for (T a : list) {
+			result += a.doubleValue();
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> ints = Arrays.asList(1, 2, 2, 3, 3, 4);
+		System.out.println("List: " + ints);
+		double intSum = sum(ints);
+		System.out.println("Sum of the list: " + intSum);
+		List<Double> doubles = Arrays.asList(2.0, 2.0, 3.0, 3.0, 4.0);
+		System.out.println("List: " + doubles);
+		double dbSum = sum(doubles);
+		System.out.println("Sum of the list: " + dbSum);
 	}
 
 }
